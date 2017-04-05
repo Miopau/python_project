5385c3909533c8358981f9924512ae5f2d56c4d3
Florian VALLET <E155051J@U-INF-J-E117-19.iut-nantes.univ-nantes.prive>
2017-04-05 15:58:59 +0200

    database

--- src/create_db.py
+++ src/create_db.py
@@ -5,46 +5,70 @@ import sqlite3, os
 
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    conn = sqlite3.connect('{}/../data/project_database.db'.format(dir_path))
-    cursor = conn.cursor()
+    try:
+        conn = sqlite3.connect('{}/../data/project_database.db'.format(dir_path))
+        cursor = conn.cursor()
 
-    #cleaning database
-    clean(cursor)
+        #cleaning database
+        clean(cursor)
 
-    #create TABLE
-    create(cursor)
+        #create TABLE
+        create(cursor)
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+    except Exception as e:
+        print("ERREUR:")
+        print (e)
+        print ("\n")
+
+    finally:
+        conn.close()
+
 
 def clean(cursor):
     cursor.execute("""DROP TABLE IF EXISTS equipement_activite""")
-    cursor.execute("""DROP TABLE IF EXISTS installations""")
+    cursor.execute("""DROP TABLE IF EXISTS installation""")
     cursor.execute("""DROP TABLE IF EXISTS equipement""")
     cursor.execute("""DROP TABLE IF EXISTS activite""")
 
 def create(cursor):
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS activite(
+    CREATE TABLE IF NOT EXISTS equipement_activite(
          id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-         name TEXT,
-         age INTERGER
-    )
-    """)
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS equipement(
-         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-         name TEXT,
-         age INTERGER
+         numero_equipement INTEGER,
+         numero_activite INTERGER,
+         FOREIGN KEY(numero_equipement) REFERENCES equipement(numero),
+         FOREIGN KEY(numero_activite) REFERENCES activite(numero)
     )
     """)
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS installation(
          id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-         name TEXT,
-         age INTERGER
+         numero INTEGER,
+         nom TEXT,
+         adresse TEXT,
+         code_postal TEXT,
+         ville TEXT,
+         latitude DECIMAL,
+         longitude DECIMAL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS equipement(
+         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+         nom TEXT,
+         numero_installation INTERGER,
+         FOREIGN KEY(numero_installation) REFERENCES installation(numero)
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS activite(
+         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+         numero INTEGER,
+         nom TEXT
     )
     """)
 
