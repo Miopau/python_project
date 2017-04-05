2b27812c68fa1f70ea898ad8991f6e0490d0fd3f
Matthieu BERNARD <E145757G@U-INF-J-E117-13.iut-nantes.univ-nantes.prive>
2017-04-05 16:54:04 +0200

    Update classes

--- src/activite.py
+++ src/activite.py
@@ -3,10 +3,16 @@
 import csv
 
 class Activity:
+    """
+    Variable and class declaration
+    """
     activityID = 0
     activityName = ""
 
     def __init__(self, activityID, activityName):
+        """
+        Construct function of the class
+        """
         self.code_postale = code_postale
         self.nom_commune = nom_commune
         self.equipement_id = equipement_id
@@ -14,10 +20,15 @@ class Activity:
         self.activityID = activityID
         self.activityName = activityName
 
-activiteList = []
+    #Initialisation of the list
+    activityList = []
 
-def readCSV():
-    with open("../initial/activites.csv","r") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            activiteList.append(Activite(row[0],row[1],row[2],row[3],row[4],row[5]));
+    def readCSV():
+        """
+        Function that read the csv and create Activity list
+        """
+        with open("../initial/activites.csv","r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                activiteList.append(Activite(row[0],row[1],row[2],row[3],row[4],row[5]));
+            return activityList #Return the list of activity
--- src/installations.py
+++ src/installations.py
@@ -3,8 +3,10 @@
 #Importation of the csv library
 import csv
 
-#Variable declaration
 class Installation:
+    """
+    Variable and class declaration
+    """
     installationName = ""
     installation_id = 0
     cityName = ""
@@ -14,8 +16,10 @@ class Installation:
     latitude = 0
     disabledAdapted = False
 
-    #Construc function of the class
     def __init__(self, installationName, installation_id, cityName, zipCode, installationAdress, longitude, latitude, disabledAdapted):
+        """
+        Construct function of the class
+        """
         self.installationName = installationName
         self.installation_id = installation_id
         self.cityName = cityName
@@ -25,14 +29,15 @@ class Installation:
         self.latitude = latitude
         self.disabledAdapted = disabledAdapted
 
-#Initialisation of the list
-installationList = []
+    #Initialisation of the list
+    installationList = []
 
-#Function that read the csv and create Installation object
-def readCSV():
-    with open("../initial/installations.csv","r") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            installationList.append(installation(row[0],row[1],row[2],row[4],row[5]+" "+row[6],row[9],row[10], row[12]));
-        for elt in installationList:
-            elt.toString();
+    def readCSV():
+        """
+        Function that read the csv and create Equipment list
+        """
+        with open("../initial/installations.csv","r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                installationList.append(installation(row[0],row[1],row[2],row[4],row[5]+" "+row[6],row[9],row[10], row[12]));
+            return installationList #Return the list of installation
