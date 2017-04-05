6123358deefceb8b32a0c182bd4c55103d35ec4b
Matthieu BERNARD <E145757G@U-INF-J-E117-13.iut-nantes.univ-nantes.prive>
2017-04-05 14:40:14 +0200

    Update of file

--- src/activite.py
+++ src/activite.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import csv
 
@@ -7,6 +7,10 @@ class Activity:
     activityName = ""
 
     def __init__(self, activityID, activityName):
+        self.code_postale = code_postale
+        self.nom_commune = nom_commune
+        self.equipement_id = equipement_id
+        self.nb_equipement = nb_equipement
         self.activityID = activityID
         self.activityName = activityName
 
@@ -16,4 +20,4 @@ def readCSV():
     with open("../initial/activites.csv","r") as file:
         reader = csv.reader(file)
         for row in reader:
-            activiteList.append(Activite(row[4],row[5]));
+            activiteList.append(Activite(row[0],row[1],row[2],row[3],row[4],row[5]));
